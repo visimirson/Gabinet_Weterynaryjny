f31dffdef3119e67a34e6e9e1d84795dd1577898
Szymon <visimir@outlook.com>
2024-01-10 09:06:16 +0100

    footer added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const nav = document.querySelector('.nav__items');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
 const navBtnBars = document.querySelector('.burger-btn__bars');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
     nav.classList.toggle('nav--active');
@@ -13,4 +14,9 @@ const handleNav = () => {
     })
 };
 
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
